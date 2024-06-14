6a8868871948e0c92ae4fed7825a416fcb1cd536
justArale <just1arale@gmail.com>
2024-06-14 22:25:01 +0200

    setup homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import GamePage from "./GamePage";
-import RecipeBookPage from "./RecipeBookPage";
-import SpotifyProjectPage from "./SpotifyProjectPage";
 import Navbar from "../components/Navbar";
 import "../components/Homepage.css";
 import { Link } from "react-router-dom";
@@ -26,9 +23,45 @@ function HomePage() {
             </p>
           </div>
         </Link>
-        <SpotifyProjectPage />
-        <RecipeBookPage />
-        <GamePage />
+        <Link to="/spotify-project" className="noUnderline">
+          <div className="spotifyFiller projectBox">
+            <div className="header">
+              <div className="iconWrapper">
+                <p className="bodyLarge">🎧</p>
+              </div>
+              <h1 className="bodyLarge">Spotify Project</h1>
+            </div>
+            <p className="body spotify">
+              Create a playlist based on your fav artist and mood.
+            </p>
+          </div>
+        </Link>
+        <Link to="/recipe-book" className="noUnderline">
+          <div className="recipebookFiller projectBox">
+            <div className="header">
+              <div className="iconWrapper">
+                <p className="bodyLarge">📖</p>
+              </div>
+              <h1 className="bodyLarge">Recipe Book</h1>
+            </div>
+            <p className="body recipebook">
+              My very personal collection of favourite recipes.
+            </p>
+          </div>
+        </Link>
+        <Link to="/game" className="noUnderline">
+          <div className="gameFiller projectBox">
+            <div className="header">
+              <div className="iconWrapper">
+                <p className="bodyLarge">🕹️</p>
+              </div>
+              <h1 className="bodyLarge">Alien Invasion</h1>
+            </div>
+            <p className="body game">
+              A small browser game for desktop and also first project.
+            </p>
+          </div>
+        </Link>
       </div>
     </div>
   );
